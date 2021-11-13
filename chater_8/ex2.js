1d8e38c4eb26774aacb1241c588ed0c7eec43762
Anthony <antho.2999@gmail.com>
2021-11-13 19:00:38 -0400

    Solutions for chapter 8

---
+++ chater_8/ex2.js
@@ -0,0 +1,36 @@
+const box = {
+    locked: true,
+    unlock() { this.locked = false; },
+    lock() { this.locked = true;  },
+    _content: [],
+    get content() {
+      if (this.locked) throw new Error("Locked!");
+      return this._content;
+    }
+  };
+  
+  function withBoxUnlocked(body) {
+    let memory = box.locked;
+    try{
+        if(memory) box.unlock();
+        body();
+    } catch(e){
+        console.log(e);
+    } finally{
+        if(memory) box.lock();
+    }
+  }
+  
+  withBoxUnlocked(function() {
+    box.content.push("gold piece");
+  });
+  
+  try {
+    withBoxUnlocked(function() {
+      throw new Error("Pirates on the horizon! Abort!");
+    });
+  } catch (e) {
+    console.log("Error raised: " + e);
+  }
+  console.log(box.locked);
+  // → true
